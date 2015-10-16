2f3f854dc5a3ebb296816f5fac1f5d1902f60b02
Jarred Kenny <jarred@jarredkenny.com>
2015-10-16 09:12:30 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  entry: './lib/Main.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      }
+    ]
+  }
+};
